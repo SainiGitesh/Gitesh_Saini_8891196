45391486a74b89a4b6b849860d69c9875b4d2638
Gitesh Saini <giteshsaini45@gmail.com>
2023-06-25 00:01:21 -0400

    Update 3

--- Lab and Assignment/calculator.py
+++ Lab and Assignment/calculator.py
@@ -1,11 +1,13 @@
 def add(x,y):
-    x+y
+    return x+y
 
 def subtract(x,y):
-    x-y
+    return x-y
 
 def multiply(x,y):
-    x*y
+    return x*y
 
 def divide(x,y):
-    x/y
+    if y == 0:
+        raise ValueError("Cannot divide by zero")
+    return x/y
